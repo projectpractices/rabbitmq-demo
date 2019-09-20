1f843ab14918e9ec809354fcc823860b07a1f641
wenhao <921790476@qq.com>
2019-09-21 00:38:34 +0800

    路由模式

---
+++ src/main/java/com/wenhao/rabbitmq/demo/recived/RecivedRouting001.java
@@ -0,0 +1,74 @@
+package com.wenhao.rabbitmq.demo.recived;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class RecivedRouting001 {
+    /**
+     * 队列名称
+     */
+    private static final String QUEUE_INFORM_EMAIL = "queue_routing_inform_email";
+    private static final String EXCHANGE_ROUTING_INFORM = "exchange_routing_inform";
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory;
+        Connection connection;
+        try {
+            connectionFactory = new ConnectionFactory();
+            connectionFactory.setHost("39.104.50.29");
+            connectionFactory.setPort(7007);
+            connectionFactory.setUsername("rabbitmq");
+            connectionFactory.setPassword("rabbitmq");
+            connectionFactory.setVirtualHost("/");
+            connection = connectionFactory.newConnection();
+            Channel channel = connection.createChannel();
+            /**
+             * 声明交换机
+             * param1:交换机名称
+             * param2:交换机类型
+             */
+            channel.exchangeDeclare(EXCHANGE_ROUTING_INFORM, BuiltinExchangeType.DIRECT);
+            /**
+             * 声明队列:如果rabbitmq中没有 将会自动创建
+             * param1:队列名称
+             * param2:是否持久化
+             * param3:是否独占此连接
+             * param4:队列不再使用时是否自动删除
+             * param4:队列参数
+             */
+            channel.queueDeclare(QUEUE_INFORM_EMAIL, true, false, false, null);
+            /**
+             * 队列和交换机绑定
+             * param1:队列名称
+             * param2:交换机名称
+             */
+            channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_ROUTING_INFORM, QUEUE_INFORM_EMAIL);
+            DefaultConsumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    //交换机
+                    String exchange = envelope.getExchange();
+                    //路由key
+                    String routingKey = envelope.getRoutingKey();
+                    //消息标记
+                    long deliveryTag = envelope.getDeliveryTag();
+                    String message = new String(body, StandardCharsets.UTF_8);
+                    System.out.println("收到的消息为：" + message+"");
+                }
+            };
+
+            /**
+             * param1:队列名称
+             * param2:是否自动应答
+             * param3:回调方法
+             */
+            channel.basicConsume(QUEUE_INFORM_EMAIL, true, consumer);
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/wenhao/rabbitmq/demo/send/SendRouting001.java
@@ -0,0 +1,96 @@
+package com.wenhao.rabbitmq.demo.send;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 工作队列模式
+ */
+public class SendRouting001 {
+
+    /**
+     * 队列名称
+     */
+    private static final String QUEUE_INFORM_EMAIL = "queue_routing_inform_email";
+    private static final String QUEUE_INFORM_SMS = "queue_routing_inform_sms";
+    private static final String EXCHANGE_ROUTING_INFORM = "exchange_routing_inform";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            //创建工厂连接
+            ConnectionFactory factory = new ConnectionFactory();
+            //设置队列服务器地址
+            factory.setHost("39.104.50.29");
+            //设置端口
+            factory.setPort(7007);
+            //设置用户名
+            factory.setUsername("rabbitmq");
+            //设置密码
+            factory.setPassword("rabbitmq");
+            //设置虚拟主机
+            factory.setVirtualHost("/");
+            //创建连接
+            connection = factory.newConnection();
+            //创建与exchange的通道,每个连接可以创建多个通道,每个通道代表一个会话任务
+            channel = connection.createChannel();
+            /**
+             * 声明交换机
+             * param1:交换机名称
+             * param2:交换机类型
+             */
+            channel.exchangeDeclare(EXCHANGE_ROUTING_INFORM, BuiltinExchangeType.DIRECT);
+            /**
+             * 声明队列:如果rabbitmq中没有 将会自动创建
+             * param1:队列名称
+             * param2:是否持久化
+             * param3:是否独占此连接
+             * param4:队列不再使用时是否自动删除
+             * param4:队列参数
+             */
+            channel.queueDeclare(QUEUE_INFORM_EMAIL, true, false, false, null);
+            channel.queueDeclare(QUEUE_INFORM_SMS, true, false, false, null);
+            /**
+             * 队列和交换机绑定
+             * param1:队列名称
+             * param2:交换机名称
+             */
+            channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_ROUTING_INFORM, QUEUE_INFORM_EMAIL);
+            channel.queueBind(QUEUE_INFORM_SMS, EXCHANGE_ROUTING_INFORM, QUEUE_INFORM_SMS);
+            /**
+             * 发布消息
+             * 发布到不存在的交换机将会收到一个通道级别的协议异常,从而导致关闭改通道
+             *  param1:消息发送至哪个交换机
+             *  param2:路由key,交换机把消息转发至对应的队列
+             *  param3:消息的属性
+             *  param4:消息体
+             */
+            for (int i = 0; i < 12; i++) {
+                String message = "hello rabbitmq"+ i;
+                channel.basicPublish(EXCHANGE_ROUTING_INFORM, QUEUE_INFORM_EMAIL, null, message.getBytes());
+                System.out.println("发送的消息为:=" + message);
+            }
+            for (int i = 0; i < 3; i++) {
+                String message = "hello rabbitmq"+ i;
+                channel.basicPublish(EXCHANGE_ROUTING_INFORM, QUEUE_INFORM_SMS, null, message.getBytes());
+                System.out.println("发送的消息为:=" + message);
+            }
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            if (channel != null) {
+                channel.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+    }
+}
